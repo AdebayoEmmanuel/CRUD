e856b78bae5434a259bfd45ff5bddba7a25f3521
Aiyegbayo Bolaji <aiyegbayo17@gmail.com>
2021-10-10 09:39:06 +0100

    bj

---
+++ config.js
@@ -0,0 +1,16 @@
+//  Configure this style to suit data handling procedures
+exports.config = {
+    "auth": {
+        "type": 0,
+        "host": "localhost",
+        "user": "root",
+        "password": "",
+        "port": "3306",
+        "database": 0
+    },
+    "options": {
+        "database": "",
+        "table": "",
+        "operations": ""
+    }
+}
